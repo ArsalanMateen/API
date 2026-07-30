6143aca48d98f8057e78f5b77560d4bf920830e7
Arsalan Mateen <arsalanmateen.pk@gmail.com>
2026-07-30 14:45:31 +0500

    read endpoints with 404

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.json({
-       'name': 'Task API',
-       'verson':'1.0',
-       'endpoints':['/tasks']
-    });
+// in-memory list of tasks objects
+const tasks = [
+  {
+    id: 1,
+    title: "FlyRank Week 1 Assignment",
+    mark_as_done: false,
+  },
+  {
+    id: 2,
+    title: "Feedback on Startup Proposal",
+    mark_as_done: false,
+  },
+  {
+    id: 3,
+    title: "ML freecodecamp Playist",
+    mark_as_done: false,
+  },
+];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
 
-app.get('/health', (req, res) =>{
-    res.json({
-        'status' : 'ok'
-    })
-} );
+app.get("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  for (const task of tasks) {
+    if (task.id === id) {
+      return res.status(200).json(task);
+    }
+  }
+
+  res.status(404).json({
+    message: `Task ${id} not found`,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Task API",
+    verson: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Application listening on port ${port}`);
 });
